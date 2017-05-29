92b21e5440a98c75b1b71a0dc55a3aa0024c369e
Juho Vepsalainen <bebraw@gmail.com>
2017-05-29 17:15:53 +0300

    chore: Add minifying chapter code

---
+++ optimizing/01-minifying/webpack-demo/app/index.js
@@ -0,0 +1,7 @@
+import 'react';
+import 'font-awesome/css/font-awesome.css';
+import 'purecss';
+import './main.css';
+import component from './component';
+
+document.body.appendChild(component());
