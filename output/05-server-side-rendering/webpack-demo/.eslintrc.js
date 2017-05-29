c6a4aac6597e1364a6e2406953b55b4832a0501f
Juho Vepsalainen <bebraw@gmail.com>
2017-05-29 17:52:21 +0300

    chore: Add SSR chapter code

---
+++ output/05-server-side-rendering/webpack-demo/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+
+    // Enable JSX
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': ['warn'],
+    'no-console': 0,
+  },
+};
---
+++ output/05-server-side-rendering/webpack-demo/webpack.config.js
@@ -0,0 +1,131 @@
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
+const glob = require('glob');
+
+const parts = require('./webpack.parts');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+};
+
+const commonConfig = merge([
+  {
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+  },
+  parts.lintJavaScript({ include: PATHS.app }),
+  parts.lintCSS({ include: PATHS.app }),
+  parts.loadFonts({
+    options: {
+      name: '[name].[hash:8].[ext]',
+    },
+  }),
+  parts.loadJavaScript({ include: PATHS.app }),
+]);
+
+const productionConfig = merge([
+  {
+    performance: {
+      hints: 'warning', // 'error' or false are valid too
+      maxEntrypointSize: 100000, // in bytes
+      maxAssetSize: 450000, // in bytes
+    },
+    output: {
+      chunkFilename: '[name].[chunkhash:8].js',
+      filename: '[name].[chunkhash:8].js',
+    },
+    plugins: [
+      new webpack.HashedModuleIdsPlugin(),
+    ],
+    recordsPath: path.join(__dirname, 'records.json'),
+  },
+  parts.clean(PATHS.build),
+  parts.minifyJavaScript(),
+  parts.minifyCSS({
+    options: {
+      discardComments: {
+        removeAll: true,
+      },
+      // Run cssnano in safe mode to avoid
+      // potentially unsafe transformations.
+      safe: true,
+    },
+  }),
+  parts.generateSourceMaps({ type: 'source-map' }),
+  parts.extractCSS({
+    use: ['css-loader', parts.autoprefix()],
+  }),
+  parts.purifyCSS({
+    paths: glob.sync(`${PATHS.app}/**/*.js`, { nodir: true }),
+  }),
+  parts.loadImages({
+    options: {
+      limit: 15000,
+      name: '[name].[hash:8].[ext]',
+    },
+  }),
+  parts.extractBundles([
+    {
+      name: 'vendor',
+      minChunks: ({ resource }) => (
+        resource &&
+        resource.indexOf('node_modules') >= 0 &&
+        resource.match(/\.js$/)
+      ),
+    },
+    {
+      name: 'manifest',
+      minChunks: Infinity,
+    },
+  ]),
+  parts.attachRevision(),
+  parts.setFreeVariable(
+    'process.env.NODE_ENV',
+    'production'
+  ),
+]);
+
+const developmentConfig = merge([
+  {
+    output: {
+      devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]',
+    },
+  },
+  parts.generateSourceMaps({ type: 'cheap-module-eval-source-map' }),
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+  parts.loadCSS(),
+  parts.loadImages(),
+]);
+
+module.exports = (env) => {
+  const pages = [
+    parts.page({
+      title: 'Webpack demo',
+      entry: {
+        app: PATHS.app,
+      },
+      chunks: ['app', 'manifest', 'vendor'],
+    }),
+    parts.page({
+      title: 'Another demo',
+      path: 'another',
+      entry: {
+        another: path.join(PATHS.app, 'another.js'),
+      },
+      chunks: ['another', 'manifest', 'vendor'],
+    }),
+  ];
+  const config = env === 'production' ?
+    productionConfig :
+    developmentConfig;
+
+  return merge([commonConfig, config].concat(pages));
+};
